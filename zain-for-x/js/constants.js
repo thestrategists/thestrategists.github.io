bf5fa7f781704f6dd020d4a3350ede86f0885edd
thestrategists <131897759+thestrategists@users.noreply.github.com>
2023-04-26 10:12:00 -0600

    Add files via upload

---
+++ zain-for-x/js/constants.js
@@ -0,0 +1,2 @@
+export const DEFAULT_GENERATED_FILE_NAME = 'meme.png';
+export const ACCEPTED_MIME_TYPES = ['image/jpg', 'image/jpeg', 'image/png', 'image/apng', 'image/gif', 'image/webp', 'image/avif'];
